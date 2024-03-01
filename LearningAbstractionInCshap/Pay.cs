f2b0bcf6be35568d48af58624d6ea7502437339b
USER <USER@DESKTOP-GSI7M8Q>
2024-03-01 16:15:08 +0100

    Add project files.

---
+++ LearningAbstractionInCshap/Pay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningAbstractionInCshap
+{
+    public abstract class Pay
+    {
+        protected int _basicPay = 50000;
+        protected int _houseRent = 89000;
+        protected int _payRate = 5;
+        protected int _netPay;
+
+        public abstract int GradeOne { get;  }
+
+        public abstract int GradeTwo { get;  }
+    }
+
+    public class PayBill : Pay
+    {
+        public override int GradeOne {
+            get
+            {
+                return _netPay = _basicPay + _houseRent;
+            }
+                }
+        public override int GradeTwo {
+            get
+            {
+                return _netPay = _basicPay+_basicPay/_payRate + _houseRent * _payRate;
+            }
+            }
+        public void display()
+        {
+            Console.WriteLine("The output Maryann was able to get for GradeOne is : {0}",
+                  GradeOne);
+            Console.WriteLine("The output Maryann was able to get for GradeTwo is : {0}",
+                  GradeTwo);
+        }
+    }
+   
+}
+
+        
+      
